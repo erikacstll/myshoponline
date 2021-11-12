9ce39dbd825945f8cab63715824df1d437f5aaa8
erikacstll62@gmail.com <erikacstll62@gmail.com>
2021-11-11 23:36:25 -0300

    Commit Button count

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import CarWidget from "./components/CarWidget/CarWidget";
 import Index from "./components/Index";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <><header>
-      <div><NavBar></NavBar></div>
+      <div>
+      <NavBar/>
+      </div>
+  
 
       <div class="header">
         <h1 class="header__Titulo1"> MyShopOnline </h1>
         <p>Take a look at some of latest tech finds </p>
       </div>
+      <CarWidget/>
     </header>
     <main>
         <h1>Welcome</h1>
+  function Catalog(params) {
+    
+  }
+  function ItemCount(monitor) { stock="9", initial="1"
+    
+  }
       </main></>
   );
 }
