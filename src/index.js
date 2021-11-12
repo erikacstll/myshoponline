c1e3404bbb6d0ef39c02b56e9f7eea768441f593
erikacstll62@gmail.com <erikacstll62@gmail.com>
2021-11-11 23:50:29 -0300

    commit 20

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import bootstrap 'bootstrap/dist/bootstrap.min.css';
-
-ReacommitctDOM.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>,
-    document.getElementById('root')
-);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
