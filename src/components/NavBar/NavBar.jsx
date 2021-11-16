d4d58960de9a6202d94c357e8a142439d5a72427
erikacstll62@gmail.com <erikacstll62@gmail.com>
2021-11-16 02:17:18 -0300

    >shell

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import React, { Component } from 'react'
+
+
+function NavBar() {
+    return (
+        <div>
+            <nav>
+                <div class="topbarmenu">
+                    <ul>
+                        <li> MyShopOnline </li>
+                        <li> Discover </li>
+                        <li> Projects </li>
+                        <li> Contact </li>
+                        <li> Info </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </div>
+    )
+}
+
+export default NavBar
