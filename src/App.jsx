23af27196451ca21a8f1346fc7da45541cf17585
erikacstll62@gmail.com <erikacstll62@gmail.com>
2021-11-16 03:50:07 -0300

    commitTTT

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import CarWidget from "./components/CarWidget/CarWidget";
 import Index from "./components";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from '@mui/material/Button';
 
 
 function App() {
-  return (
-    <><header>
-      <div>
-      <NavBar/>
-      </div>
-  
+    return ( <div >
+        <div >
+        <NavBar/>
+        </div>
 
-      <div class="header">
-        <h1 class="header__Titulo1"> MyShopOnline </h1>
-        <p>Take a look at some of latest tech finds </p>
-      </div>
-      <CarWidget/>
-    </header>
-    <main>
-        <h1>Welcome</h1>
-  function Catalog(params) {
-    
-  }
-  function ItemCount(monitor) { stock="9", initial="1"
-    
-  }
-      </main></>
-  );
+
+        <div class = "header" >
+        <h1 class = "header__Titulo1" > MyShopOnline </h1> 
+        <p> Take a look at some of latest tech finds </p> 
+        </div> 
+        <CarWidget/>
+
+        <main>
+        <h1> Welcome </h1> function Catalog(params) {
+
+        }
+
+        function ItemCount(monitor) {
+            stock = "9", initial = "1"
+
+        } </main> 
+        </div> 
+        );
 }
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import React, { Component } from 'react'
+import { Component } from 'react';
 
 
-function NavBar() {
-    return (
-        <div>
+class NavBar extends Component {
+    render() {
+        return <div>
             <nav>
-                <div class="topbarmenu">
-                    <ul>
-                        <li> MyShopOnline </li>
-                        <li> Discover </li>
-                        <li> Projects </li>
-                        <li> Contact </li>
-                        <li> Info </li>
-                    </ul>
-                </div>
+            <div class = "topbarmenu">
+            <ul>
+            <li> MyShopOnline </li> 
+            <li> Discover </li> 
+            <li> Projects </li> 
+            <li> Contact </li> 
+            <li> Info </li> 
+            </ul>
+            </div>
             </nav>
 
-        </div>
-    )
+        </div>;
+    }
 }
 
-export default NavBar
+export default NavBar;
